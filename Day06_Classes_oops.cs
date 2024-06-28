68aa9aaf0fdf05ec835014562ab8610535723e54
Gatecrasher027 <114968405+Gatecrasher027@users.noreply.github.com>
2024-06-27 23:49:51 -0400

    Update Day06_Classes_oops.cs

--- Day06_Classes_oops.cs
+++ Day06_Classes_oops.cs
@@ -1,1 +1,15 @@
-/*Question 1: */
+/*Question: Write a program that that demonstrates use of four basic principles of
+object-oriented programming /Abstraction/, /Encapsulation/, /Inheritance/ and
+/Polymorphism/
+2. Use /Abstraction/ to define different classes for each person type such as Student
+and Instructor. These classes should have behavior for that type of person.
+3. Use /Encapsulation/ to keep many details private in each class.
+4. Use /Inheritance/ by leveraging the implementation already created in the Person
+class to save code in Student and Instructor classes.
+5. Use /Polymorphism/ to create virtual methods that derived classes could override to
+create specific behavior such as salary calculations.
+6. Make sure to create appropriate /interfaces/ such as ICourseService, IStudentService,
+IInstructorService, IDepartmentService, IPersonService, IPersonService (should have
+person specific methods). IStudentService, IInstructorService should inherit from
+IPersonService.*/
+
